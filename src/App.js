bc7b1ffcd732f501e9c08249859cf1d1734b156b
aisha-alimi <aisha.a.alimi@gmail.com>
2020-05-16 12:56:49 +0200

    hello from feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey there!
+          Hey there! - from feature
         </p>
         <a
           className="App-link"
